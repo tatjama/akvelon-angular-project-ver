37cb1937feb81f9d74494672911bde465ef64e28
Tanja <tanja120a@gmail.com>
2021-04-14 19:27:30 +0200

    Updated styles
    nav, list, task change width to 40%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
 
 const routes: Routes = [
-  {path: 'projects', component: ProjectsComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: "full"},
-  {path: 'projectdetail/:id', component: ProjectDetailComponent}
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'projects/:id', component: ProjectDetailComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: "full"}
 ];
 
 @NgModule({
